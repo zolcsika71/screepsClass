d6d91c87e52ec9502b1610a2f2ded143086f496d
zolcsika71 <bushdoctor2008@gmail.com>
2019-10-25 16:01:06 +0200

    first

---
+++ notes/modules/import as.js
@@ -0,0 +1,22 @@
+import {aircrafts, flightReqs, meetsStaffReqs, meetsSpeedRangeReqs } from './export as';
+function displayFuelCapacity() {
+    aircrafts.forEach(element => {
+        console.log(`Fuel Capacity of ${element.name}:  ${element.fuelCapacity}`);
+    })
+}
+displayFuelCapacity();
+
+function displayStaffStatus() {
+    aircrafts.forEach(function(element) {
+        console.log(element.name + ' meets staff requirements: ' + meetsStaffReqs(element.availableStaff, flightReqs.requiredStaff) );
+    });
+}
+displayStaffStatus();
+
+function displaySpeedRangeStatus() {
+    aircrafts.forEach(function(element) {
+        console.log(element.name + ' meets speed range requirements: ' + meetsSpeedRangeReqs(element.maxSpeed, element.minSpeed, flightReqs.requiredSpeedRange));
+    });
+}
+
+displaySpeedRangeStatus();
--- notes/objects/assign_newProp_witout_change_originalObject.js
+++ notes/objects/assign_newProp_witout_change_originalObject.js
@@ -1,24 +1,5 @@
-const robot = {
-    model: 'SAL-1000',
-    mobile: true,
-    sentient: false,
-    armor: 'Steel-plated',
-    energyLevel: 75
-};
-
-// What is missing in the following method call?
-const robotKeys = Object.keys(robot);
-
-console.log(robotKeys);
-
-// Declare robotEntries below this line:
-
-const robotEntries = Object.entries(robot);
-console.log(robotEntries);
-
-// Declare newRobot below this line:
-
-const newRobot = Object.assign({laserBlaster: true, voiceRecognition: true}, robot);
+const Menu = require('./1-airplane.js');
+function displayAirplane() {
 
 
-console.log(newRobot);
+}
