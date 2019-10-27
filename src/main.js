59328545109d22292a275633e466ded0c3789a25
zolcsika71 <bushdoctor2008@gmail.com>
2019-10-27 17:37:06 +0100

    first

--- src/main.js
+++ src/main.js
@@ -1,3 +1,30 @@
+"use strict";
+function wrapLoop(fn) {
+    let memory,
+        tick;
 
+    return () => {
+        if (tick && tick + 1 === Game.time && memory) {
+            delete global.Memory;
+            Memory = memory;
+        } else
+            memory = Memory;
+
+        tick = Game.time;
+        fn();
+
+        // there are two ways of saving Memory with different advantages and disadvantages
+        // 1. RawMemory.set(json.stringify(Memory));
+        // + ability to use custom serialization method
+        // - you have to pay for serialization
+        // - unable to edit Memory via Memory watcher or console
+        // 2. RawMemory._parsed = Memory;
+        // - undocumented functionality, could get removed at any time
+        // + the server will take care of serialization, it doesn't cost any CPU on your site
+        // + maintain full functionality including Memory watcher and console
+
+        RawMemory._parsed = Memory;
+    };
+}
 
 
