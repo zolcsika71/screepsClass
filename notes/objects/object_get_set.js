2cb6da1b93a9781bdf23befca12b36bab076063c
zolcsika71 <bushdoctor2008@gmail.com>
2020-03-28 17:39:42 +0100

    first

--- notes/objects/object_get_set.js
+++ notes/objects/object_get_set.js
@@ -14,7 +14,7 @@ const robot = {
         if (typeof num === 'number' && num >= 0)
             this._numOfSensors = num;
         else
-            console.log('Pass in a number that is greater than or equal to 0');;
+            console.log('Pass in a number that is greater than or equal to 0');
     }
 
 };
